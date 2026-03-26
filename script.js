906ca7c5ce2a58efa5449aed623cbc257890d8f7
amarsriram <amarsriram.work@gmail.com>
2026-03-26 15:10:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ function generate() {
   const service = document.getElementById("service").value;
   const phone = document.getElementById("phone").value;
 
-  document.getElementById("output").innerHTML = `
-    <h2>${name}</h2>
-    <p>${service}</p>
-    <a href="tel:${phone}">Call Now</a><br>
-    <a href="https://wa.me/${phone}">WhatsApp</a>
+  const result = `
+<h2>${name}</h2>
+<p>${service}</p>
+<a href="tel:${phone}">Call Now</a><br>
+<a href="https://wa.me/${phone}">WhatsApp</a>
   `;
+
+  document.getElementById("output").innerHTML = `
+    ${result}
+    <br><br>
+    <button onclick="copyText()">Copy Code</button>
+  `;
+
+  window.generatedCode = result;
+}
+
+function copyText() {
+  navigator.clipboard.writeText(window.generatedCode);
+  alert("Copied!");
 }
